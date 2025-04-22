7137dd39a98bf744a1421c46aa9fb32c1078748c
delwin3000 <165779020+delwin3000@users.noreply.github.com>
2025-04-23 05:15:50 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = 'super-secret-key'  # Change this later
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
- 
